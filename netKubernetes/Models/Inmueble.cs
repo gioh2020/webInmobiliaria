2c85109d31797c4484f65bdbc0999a7fc8ce580f
gioh <gioh2020@gmail.com>
2024-02-17 15:04:30 -0500

    devGioh/se agregan los modelos en .net

---
+++ netKubernetes/Models/Inmueble.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace netKubernetes.Models
+{
+    public class Inmueble
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+
+        [Required]
+        [Column(TypeName = "Decimal(18,4)")]
+        public decimal Precio { get; set; }
+        public string? Picture { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+    }
+}
---
+++ netKubernetes/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace netKubernetes.Models
+{
+    public class Usuario: IdentityUser
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Telefono { get; set; }
+    }
+}
