62ce850b8a16dea05f237b14437e213a12565eda
gioh <gioh2020@gmail.com>
2024-02-17 15:07:28 -0500

    devGioh/configuraciones iniciales

---
+++ netKubernetes/Token/UsuarioSesion.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace netKubernetes.Token
+{
+    public class UsuarioSesion : IUsuarioSesion
+    {
+         private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UsuarioSesion(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string ObtenerUsuarioSesion()
+        {
+            var userName = _httpContextAccessor.HttpContext!.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            return userName!;
+       
+        }
+    }
+}
